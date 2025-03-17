192f85abc616d332a37fc20751db6d0f243c7fb1
Kaleab Endrias <kaleabendrias1212@gmail.com>
2025-03-17 12:41:45 +0300

    Restrict CORS to allow requests only from localhost:5173

--- back/index.js
+++ back/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 
